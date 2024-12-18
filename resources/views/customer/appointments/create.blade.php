839141e478defcab3a44db684096dd503d66e6a6
MuhmadUmar <muhmdumar75@gmail.com>
2024-12-19 00:27:03 +0500

    reminder duration and fixes

--- resources/views/customer/appointments/create.blade.php
+++ resources/views/customer/appointments/create.blade.php
@@ -136,6 +136,8 @@
         <div class="row g-0">
             <!-- Left Sidebar -->
             <div class="col-md-4 event-details">
+
+                
                 <div class="event-icon">
                     <i class="fs-4">E</i>
                 </div>
@@ -158,14 +160,14 @@
                         {{-- We can bring your web design vision and ideas to life through innovative custom design for your business. We work on hundreds of websites a year, and bring unique insight from our deep experience. --}}
                     </span>
                 </div>
-                @auth('customers')
+                {{-- @auth('customers')
                     <div class="mt-3">
                         <strong>Name:</strong> {{$customer->name}}<br>
                         <strong>Email:</strong> {{$customer->email}}
                     </div>
                 @else
                
-                @endauth
+                @endauth --}}
 
 
                 
--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -6,6 +6,6 @@
         <h1 class="display-5 fw-bold">Dashboard</h1>
         <p class="text-muted mt-1s">View your account statistics</p>
     </div>
-
+    <iframe src="http://localhost:8000/embed/shop" width="100%" height="600" frameborder="0" style="border: 2px solid #ddd; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 10px;"></iframe>
 </main>
 @endsection
